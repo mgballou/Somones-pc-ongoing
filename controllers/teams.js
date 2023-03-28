64f515dcfd09dd343a35c1b9947fcc10fcaed674
mgballou <matthewbuiltthat@gmail.com>
2023-03-28 14:34:43 -0400

    Create index and new functions in teams controller

---
+++ controllers/teams.js
@@ -0,0 +1,20 @@
+const Team = require('../models/Team')
+
+
+
+module.exports = {
+    index,
+    new: newTeam
+}
+
+function index(req, res, next) {
+    res.render('teams/index', {title: 'All Teams'})
+
+
+}
+
+function newTeam(req, res, next){
+    res.render('teams/new', {title: 'Create new team'})
+
+
+}
