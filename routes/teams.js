42adb1d579699671aade71d8aeb2cd8cd122ec27
mgballou <matthewbuiltthat@gmail.com>
2023-03-28 14:26:54 -0400

    Create index and new routes in teams router

---
+++ routes/teams.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const teamsCtrl = require('../controllers/teams')
+
+
+router.get('/', teamsCtrl.index)
+
+router.get('/new', teamsCtrl.new)
+
+module.exports = router
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ require('./config/passport')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const teamsRouter = require('./routes/teams')
 
 const app = express();
 
@@ -42,6 +43,7 @@ app.use(function(req, res, next){
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/teams', teamsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
