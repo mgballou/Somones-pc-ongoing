3e8e745be7b5c7b2cb4742bf4c51c030ec37dcdb
mgballou <matthewbuiltthat@gmail.com>
2023-03-30 14:03:16 -0400

    Added notes and comments

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -6,6 +6,8 @@ router.post('/teams/:id/pokemon', pokemonCtrl.create)
 
 router.delete('/pokemon/:id', pokemonCtrl.delete)
 
+// router.get('/pokemon/:id', pokemonCtrl.edit))
+
 router.put('/pokemon/:id', pokemonCtrl.update)
 
 module.exports = router
