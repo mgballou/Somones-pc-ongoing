409b746b84483e31f9123eb0c2ee012a4a2131be
mgballou <matthewbuiltthat@gmail.com>
2023-04-11 16:28:31 -0400

    Create nature data model & update seed.js to pull and seed database with all natures

--- seed.js
+++ seed.js
@@ -6,51 +6,111 @@ const Team = require('./models/Team')
 const Item = require('./models/Item')
 
 const axios = require('axios')
+const Nature = require('./models/Nature')
 
-const apiPath = 'https://pokeapi.co/api/v2/item-attribute/7/'
+const apiPath = 'https://pokeapi.co/api/v2/'
+const itemEndpoint = 'item-attribute/7/'
+const naturesEndpoint = 'nature/?limit=100'
+
 
 let allItems = []
 
+let allNatures = []
+let naturesData
 
-function getAllItems(){
-    axios.get(apiPath)
-    .then(function(response){
-        
-        allItems = response.data.items.map(item => {
-            return {
-                name: item.name
-            }
+function getAllItems() {
+    axios.get(apiPath + itemEndpoint)
+        .then(function (response) {
+
+            allItems = response.data.items.map(item => {
+                return {
+                    name: item.name
+                }
+            })
+            console.log(allItems)
+            return allItems
         })
-        console.log(allItems)
-        return allItems
-    })
-    .catch(function(err){
-        console.log(err)
-    })
+        .catch(function (err) {
+            console.log(err)
+        })
 
 }
 
-function seedDatabase(){
+
+function getNatureData() {
+    axios.get(apiPath + naturesEndpoint)
+        .then(function (response) {
+            naturesData = response.data.results
+            return getNestedNaturesInfo(naturesData)
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
+}
+
+function getNestedNaturesInfo(array) {
+    let promiseArray = array.map(nature => {
+        return axios.get(nature.url)
+    })
+
+    Promise.all(promiseArray)
+        .then(function (responsesArray) {
+            responsesArray.forEach(response => {
+                let natureObject = {
+                    name: response.data.name
+                }
+                if (response.data.increased_stat === null) {
+                    natureObject.statUp = 'none'
+                    natureObject.statDown = 'none'
+                } else {
+                    natureObject.statUp = response.data.increased_stat.name
+                    natureObject.statDown = response.data.decreased_stat.name
+                }
+                allNatures.push(natureObject)
+            })
+            return allNatures
+        })
+        .then(function(){
+            console.log(allNatures)
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
+
+}
+
+function seedDatabase() {
     Pokemon.deleteMany({})
-    .then(function(results){
-        console.log(results)
-    })
-    .then(function(){
-        return Team.deleteMany({})
-    })
-    .then(function(response){
-        console.log(response)
-        return Item.create(allItems)
-    })
-    .then(function(result){
-        console.log(result)
-    })
-    .catch(function(err){
-        console.log(err)
-    })
-    .finally(function(){
-        process.exit()
-    })
+        .then(function (results) {
+            console.log(results)
+        })
+        .then(function () {
+            return Team.deleteMany({})
+        })
+        .then(function (response) {
+            console.log(response)
+            return Item.create(allItems)
+        })
+        .then(function (result) {
+            console.log(result)
+            return Nature.create(allNatures)
+        })
+        .then(function(outcome){
+            console.log(outcome)
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
+        .finally(function () {
+            process.exit()
+        })
 }
+
+
 getAllItems()
+
+
+getNatureData()
+
 seedDatabase()
+
